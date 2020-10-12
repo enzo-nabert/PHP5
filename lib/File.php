30e1ebd08c00fbc67acb2525ae2042a408909af7
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2020-10-12 10:49:56 +0200

    exercice 3 (correction du 2 portable)

--- lib/File.php
+++ lib/File.php
@@ -4,8 +4,8 @@
 class File{
 
     public static function build_path($path_array) {
-        $ROOT_FOLDER = __DIR__;
         $DS = DIRECTORY_SEPARATOR;
+        $ROOT_FOLDER = __DIR__ . $DS . "..";
         return $ROOT_FOLDER. $DS . join($DS, $path_array);
     }
 }
